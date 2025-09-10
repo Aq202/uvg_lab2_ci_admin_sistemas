7917098bbe07fac38916bc6aef0573094816a1d5
Aq202 <diegoguatedb2002@gmail.com>
2025-09-10 17:13:42 -0600

    feat: add main

---
+++ main.py
@@ -0,0 +1,8 @@
+from string_utils import reverse, concat, count_vowels, is_palindrome, to_upper
+
+if __name__ == "__main__":
+    print(reverse("Hello, World!"))
+    print(concat("Hello, ", "World!"))
+    print(count_vowels("Hello, World!"))
+    print(is_palindrome("racecar"))
+    print(to_upper("hello"))
