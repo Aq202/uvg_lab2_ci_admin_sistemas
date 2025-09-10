b2f685708088117e8ecb3b5ab3ab184e251fb944
Aq202 <diegoguatedb2002@gmail.com>
2025-09-10 11:45:22 -0600

    feat: concat function

--- string_utils/concat.py
+++ string_utils/concat.py
@@ -1,2 +1,7 @@
 def concat(a, b) -> str:
-  return None
+  """Concatena dos valores (str o número)"""
+  if not isinstance(a, (str, int, float)):
+      raise TypeError(f"El valor '{a}' de tipo {type(a)} no es válido. Solo se permiten str, int o float.")
+  if not isinstance(b, (str, int, float)):
+      raise TypeError(f"El valor '{b}' de tipo {type(b)} no es válido. Solo se permiten str, int o float.")
+  return str(a) + str(b)
