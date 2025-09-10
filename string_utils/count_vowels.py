dc334a1b82c046d3a4669fbcf1f40cf8cc37c360
Aq202 <diegoguatedb2002@gmail.com>
2025-09-10 12:10:16 -0600

    fix: errors with accent

--- string_utils/count_vowels.py
+++ string_utils/count_vowels.py
@@ -2,5 +2,5 @@ def count_vowels(s) -> int:
   """Cuenta el total de vocales en la cadena"""
   if not isinstance(s, (str, int, float)):
       raise TypeError(f"El valor '{s}' de tipo {type(s)} no es válido. Solo se permiten str, int o float.")
-  vowels = "aeiou"
+  vowels = "aeiouáéíóúü"
   return sum(1 for char in str(s).lower() if char in vowels)
