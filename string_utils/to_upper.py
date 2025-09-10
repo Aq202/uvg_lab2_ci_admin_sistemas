a35e32fc28ed87e9167e025ca03cda20e826c999
Aq202 <diegoguatedb2002@gmail.com>
2025-09-10 11:37:38 -0600

    feat: to upper function

--- string_utils/to_upper.py
+++ string_utils/to_upper.py
@@ -1,2 +1,5 @@
 def to_upper(s) -> str:
-  return
+  """Convierte la cadena a mayúsculas"""
+  if not isinstance(s, (str)):
+      raise TypeError(f"El valor '{s}' de tipo {type(s)} no es válido. Solo se permiten str.")
+  return str(s).upper()
