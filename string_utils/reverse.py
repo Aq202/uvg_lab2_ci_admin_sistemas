bc6f7a1e9921db4ce8b4e269fd75bf6f3c34e77e
Aq202 <diegoguatedb2002@gmail.com>
2025-09-10 10:48:48 -0600

    feat: reverse function

--- string_utils/reverse.py
+++ string_utils/reverse.py
@@ -1,2 +1,5 @@
 def reverse(s) -> str:
-    return None
+    """Retorna la cadena en orden inverso"""
+    if not isinstance(s, (str, int, float)):
+        raise TypeError(f"El valor '{s}' de tipo {type(s)} no es válido. Solo se permiten str, int o float.")
+    return str(s)[::-1]
